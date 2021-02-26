49dbcc8f3a4856231e4a378048e1ff7906255100
izumi <zenshinkarate2002@gmail.com>
2021-02-26 18:28:22 +0900

    0226test

---
+++ resources/views/lessons.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+@section('content')
+    <!-- Bootstrapの定形コード… -->
+    <div class="card-body">
+        <div class="card-title">
+            タイトル
+        </div>
+        
+        <!-- バリデーションエラーの表示に使用-->
+        @include('common.errors')
+        <!-- バリデーションエラーの表示に使用-->
+
+        <!-- 登録フォーム -->
+        <form action="{{ url('lessons') }}" method="POST" class="form-horizontal">
+            {{ csrf_field() }}
+
+            <!-- タイトル -->
+            <div class="form-group">
+                <div class="col-sm-6">
+                    <input type="text" name="title" class="form-control">
+                </div>
+            </div>
+
+            <!-- 登録ボタン -->
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-primary">
+                        Save
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <!-- 既に登録されてるリスト -->
+
+         <!-- 現在 -->
+         @if (count($lessons) > 0)
+        <div class="card-body">
+            <div class="card-body">
+                <table class="table table-striped task-table">
+                    <!-- テーブルヘッダ -->
+                    <thead>
+                        <th>一覧</th>
+                        <th>&nbsp;</th>
+                    </thead>
+                    <!-- テーブル本体 -->
+                    <tbody>
+                        @foreach ($lessons as $lesson)
+                            <tr>
+                                <!-- タイトル -->
+                                <td class="table-text">
+                                    <div>{{ $lesson->title }}</div>
+                                </td>
+
+                                <!-- 削除ボタン -->
+                                <td>
+                                    <form action="{{ url('lesson/'.$lesson->id) }}" method="POST">
+                                        {{ csrf_field() }}
+                                        {{ method_field('delete') }}
+                                        <button type="submit" class="btn btn-danger">
+                                            削除
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,54 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
+use App\Lesson;
+use Illuminate\Http\Request;
+
+//表示
 Route::get('/', function () {
-    return view('welcome');
+    $lessons = Lesson::orderBy('created_at', 'asc')->get();
+    return view('lessons', [
+        'lessons' => $lessons
+    ]);
 });
+
+    //add
+    Route::post('/lessons', function (Request $request) {
+
+        //バリデーション
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|min:3|max:255',
+        ]);
+    
+        //バリデーション:エラー 
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+        //以下に登録処理を記述（Eloquentモデル）
+        $lessons = new Lesson;
+        $lessons->title = $request->title;
+        $lessons->text = 'test';
+        $lessons->save(); 
+        return redirect('/');
+
+    
+    
+    
+    });
+
+    //del
+    Route::delete('/lesson/{lesson}', function (Lesson $lesson) {
+        $lesson->delete();       //追加
+        return redirect('/');  //追加
+    });
+
+
+Route::get('/list', function () {
+    return view('list');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
