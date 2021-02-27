9022a389a93fbd69ae96bda6c19ef2ba5af020a7
izumi <zenshinkarate2002@gmail.com>
2021-02-27 14:01:56 +0900

    0227navi

--- routes/web.php
+++ routes/web.php
@@ -33,5 +33,17 @@ Route::post('/teachers', 'TeachersController@store');
 Route::delete('/teacher/{teacher}','TeachersController@destroy');
 
 
+//表示
+Route::get('/navitop', 'TeachersController@index');
+//更新画面
+Route::post('/navisedit/{navis}','TeachersController@edit' );
+//更新処理
+Route::post('/navis/update', 'TeachersController@update');
+//登録
+Route::post('/navis', 'TeachersController@store');
+//削除
+Route::delete('/navi/{navi}','TeachersController@destroy');
+
+
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
